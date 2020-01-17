3a4adec936365be71219bc525c75a58a1b6aec1b
Christopher Hillenbrand <chillenbrand15@gmail.com>
2020-01-17 17:48:13 -0500

    maps

--- client/src/components/pages/MapTest.js
+++ client/src/components/pages/MapTest.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 const g_apikey = "AIzaSyCR-ulCKD_elY8EERVo4GCa07_ABalJvw8";
+const g_places_api_url = "https://maps.googleapis.com/maps/api/js?key=" + g_apikey + "&libraries=places";
+//const g_places_api = require(g_places_api_url);
+const mycenter = {lat: 42.360495, lng: -71.093779 };
+
+const mapContainerStyle = {
+  height: "400px",
+  width: "800px"
+};
 
 class MapTestComponent extends React.Component {
   constructor(props) {
@@ -11,23 +19,20 @@ class MapTestComponent extends React.Component {
   }
 
   render() {
-    const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-      <GoogleMap
-        defaultZoom={15}
-        defaultCenter={{ lat: 42.360495, lng: -71.093779 }}
-      >
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-      </GoogleMap>
-    ));
-
     return (
-        <MyMapComponent
-          isMarkerShown
-          googleMapURL={"https://maps.googleapis.com/maps/api/js?key=" + g_apikey + "&v=3.exp&libraries=geometry,drawing,places"}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
+      <div className="u-flex u-flex-alignCenter u-flex-justifyCenter">
+      <LoadScript
+        googleMapsApiKey={g_apikey}
+        libraries = {["places"]}
+      >
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={18}
+          center={mycenter}
+        >
+        </GoogleMap>
+      </LoadScript>
+      </div>
     );
   }
 }
