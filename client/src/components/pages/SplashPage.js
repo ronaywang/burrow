fdbede2784f7b1359269a0674e1879a0d1e1b191
Chris Xu <chxu@mit.edu>
2020-01-17 20:30:10 -0500

    I've added in the basic HTML class structure for SplashPage.js. Note that some of the component have yet to be implemented, and are thus commented out...

--- client/src/components/pages/SplashPage.js
+++ client/src/components/pages/SplashPage.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import {Tabs} from "../modules/Tabs.js";
 import "./SplashPage.css";
 import "../../utilities.css";
-import SearchBar from "../modules/SearchBar.js";
+import {LocationSearchBar} from "../modules/SearchBar.js";
 
 class SplashTab extends Component{
   render(){
     return (
       <Tabs styleName="SplashPage">
         <div label="Room">
-          hi this is tab 0 room oc do not steal
+          {/* TODO <LocationSearchBar defaultText="Enter a city, state, etc." styleName="SplashPage" /> SplashPage-search */}
+          {/* TODO <DatePicker styleName="SplashPage" /> SplashPage-date*/}
+          <button className="SplashPage-go" value="Go!"/>
         </div>
 
         <div label="Roommate">
-          hi this is tab 1 roommate oc do not steal
+          {/* TODO <LocationSearchBar defaultText="Enter an address" styleName="SplashPage" /> SplashPage-search */}
+          {/* TODO <DatePicker styleName="SplashPage" /> SplashPage-date*/}
+          <button className="SplashPage-go" value="Go!"/>
         </div>
       </Tabs>
     )
@@ -23,11 +27,14 @@ class SplashTab extends Component{
 class SplashPage extends Component {
   render(){
     return (
-      <div>
-        <h1>Burrow</h1>
-        <h2>i'm looking for a...</h2>
-        <SplashTab />
-        <SearchBar defaultText="example search bar" />
+      <div className="SplashPage-container">
+        <div className="SplashPage-infoContainer">
+          <div className="SplashPage-taglineContainer">
+            <div className="SplashPage-logo">burrow</div>
+            <div className="SplashPage-tagline">i'm looking for a...</div>
+          </div>
+          <SplashTab />
+        </div>
       </div>
     );
   }
