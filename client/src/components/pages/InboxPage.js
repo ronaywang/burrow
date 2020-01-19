3a4015f534c93a6dee98840e943709d781599c22
Chris Xu <chxu@mit.edu>
2020-01-19 16:07:09 -0500

    Added skeletons for MainPage, ProfilePage and InboxPage

---
+++ client/src/components/pages/InboxPage.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import "../../utilities";
+import "../../utilities.css";
+
+class ProfilePage extends Component{
+  
+}
+
+export default ProfilePage;
---
+++ client/src/components/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import "../../utilities";
+import "../../utilities.css";
+
+class MainPage extends Component{
+  
+}
+
+export default MainPage;
