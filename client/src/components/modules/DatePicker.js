b08ede40726de60e8a3f77a0446d7e56997584f6
Chris Xu <chxu@mit.edu>
2020-01-21 17:07:11 -0500

    fixed broken import

--- client/src/components/modules/DatePicker.js
+++ client/src/components/modules/DatePicker.js
@@ -6,6 +6,7 @@ import { DateRangePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 import './DatePicker.css';
 import * as moment from "moment";
+import momentPropTypes from "moment";
 
 class DatePicker extends Component {
 
