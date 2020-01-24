3dc5851ec39c6af96769a4c8ecbe41c335c4978d
ronayw <rona@mit.edu>
2020-01-23 20:41:49 -0500

    Merge branch 'master' of github.com:weblab-class/ronaywang-chillenb-chrisxu3

--- client/src/components/pages/InboxPage.js
+++ client/src/components/pages/InboxPage.js
@@ -4,13 +4,59 @@ import "../../utilities";
 import "../../utilities.css";
 import "./InboxPage.css";
 
+const makeMessageNice = (message) => {
+  return (
+    <div>{message}</div>
+  );
+};
+
 class InboxPage extends Component{
   constructor(props) {
     super(props);
+    this.state = {
+      lastSearchQuery: "",
+      searchBoxContents: "",
+      chatBoxContents: "",
+      lastMessageSubmitted: "",
+      displayedMessages: [],
+    };
+    this.ChatBoxUpdate = this.ChatBoxUpdate.bind(this);
+    this.SearchBoxUpdate = this.SearchBoxUpdate.bind(this);
+    this.SearchBoxKey= this.SearchBoxKey.bind(this);
+    this.ChatBoxKey = this.ChatBoxKey.bind(this);
   }
 
   componentDidMount() {
 
+  }
+
+
+  ChatBoxUpdate(event) {
+    this.setState({chatBoxContents: event.target.value});
+  }
+
+  ChatBoxKey(event) {
+    if(event.keyCode === 13 && event.shiftKey) {
+      this.state.displayedMessages.push(this.state.chatBoxContents);
+      this.setState({
+        lastMessageSubmitted: this.state.chatBoxContents,
+        chatBoxContents: "",
+      });
+    }
+  }
+
+  SearchBoxUpdate(event) {
+    this.setState({searchBoxContents: event.target.value});
+  }
+
+  SearchBoxKey(event) {
+    if (event.keyCode === 13) {
+      this.setState({
+        lastSearchQuery: this.state.searchBoxContents,
+        searchBoxContents: "",
+      });
+      // todo: trigger a search
+    }
   }
 
   render() {
@@ -20,17 +66,24 @@ class InboxPage extends Component{
         <input className="MessageSearch"
           type="text"
           placeholder="Search messages..."
+          value={this.state.searchBoxContents}
+          onChange={this.SearchBoxUpdate}
+          onKeyDown={this.SearchBoxKey}
           />
 
       </div>
 
       <div className="Chat-container">
+        {this.state.displayedMessages.map(makeMessageNice)}
 
         <textarea
         rows="10"
         cols="30"
         className="ChatBox" 
         placeholder="Type here"
+        value={this.state.chatBoxContents}
+        onChange={this.ChatBoxUpdate}
+        onKeyUp={this.ChatBoxKey}
         />
         </div>
     </div>
