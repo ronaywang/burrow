e7566ecc44dcfcfbe915e36aa0dbe18d5b917368
Christopher Hillenbrand <chillenbrand15@gmail.com>
2020-01-28 16:24:15 -0500

    scrolling nonsense

--- client/src/components/pages/InboxPage.js
+++ client/src/components/pages/InboxPage.js
@@ -51,6 +51,8 @@ class InboxPage extends Component{
 
 
   async componentDidMount() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     document.body.classList.remove("SplashPage-body");
     let threadToMakeActive = null;
     if (has(this.props, 'userId')) {
