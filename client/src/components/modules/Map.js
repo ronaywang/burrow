de744de7879c1854e9e1c79abae6f1c63f5fe10c
Christopher Hillenbrand <chillenbrand15@gmail.com>
2020-01-28 18:19:50 -0500

    map styling

--- client/src/components/modules/Map.js
+++ client/src/components/modules/Map.js
@@ -77,7 +77,7 @@ class MapComponent extends Component {
       width = 640;
     }
     return (
-      <div id="map" className="u-flex" style={{width : width, height: height}}>
+      <div id="map" className="u-flex">
       </div>
     );
   }
