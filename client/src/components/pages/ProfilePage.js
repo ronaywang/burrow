f35156c2bfcd63709eaf43ab4d3f91013a28d401
Chris Xu <chxu@mit.edu>
2020-01-28 12:50:36 -0500

    Revert rona's changes for now, looking in to it atm

--- client/src/components/pages/ProfilePage.js
+++ client/src/components/pages/ProfilePage.js
@@ -9,52 +9,12 @@ import {get, post} from "../../utilities";
 
 
 class ProfilePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tabIndex: 0, //0, 1, or 2, depending on which tab to display
-      isYou: false,
-      firstName: "",
-      doRender: false
-    };
-  }
-
-  componentDidMount () {
-    get("/api/getthisuserinfo", {userId: this.props.userId}).then((usr) => {
-      this.setState({isYou: usr.isYou, firstName: usr.firstName, doRender: true})
-    })
-  }
+  
 
   render() {
-    if (!this.state.doRender)
-      return null;
-    let TabToDisplay;
-    switch(this.state.tabIndex) {
-      case 0:
-        TabToDisplay = UserSettings;
-        break;
-      case 1:
-        TabToDisplay = YourListings;
-        break;
-      case 2:
-      default:
-        TabToDisplay = YourListings;
-        break;
-    }
     return (
       <div className="Profile-container">
-        <span className = "ProfileBar-tabContainer">
-          <span className="ProfileBar-tab" onClick={()=>{this.setState({tabIndex: 0})}}>
-            {this.state.isYou ? "Your" : `${this.state.firstName}'s`} profile
-          </span>
-          <span className="ProfileBar-tab" onClick={()=>{this.setState({tabIndex: 1})}}>
-          {this.state.isYou ? "Your" : `${this.state.firstName}'s`} listings
-          </span>
-           {/*<span className = "ProfileBar-tab" onClick={()=>{this.setState({tabIndex: 3})}}>Your account</span> */}
-        </span>
-       <div>
-          <TabToDisplay userId={this.props.userId} key={this.props.userId}/>
-    </div>
+        <UserSettings userId={this.props.userId} key={this.props.userId}/>
       </div>
     )
   }
