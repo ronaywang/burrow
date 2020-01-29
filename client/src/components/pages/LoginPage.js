32b680cc8393140fc981854373c2683753fc6c20
ronayw <rona@mit.edu>
2020-01-28 23:00:05 -0500

    changed flavor text

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -87,7 +87,7 @@ handleSubmit = (event) => {
               />
             </label>
             <button
-              type="submit" onClick={this.handleSubmit}>
+              type="submit"  onClick={this.handleSubmit}>
                 {this.state.buttontext}</button>
           </form>
         </div>
